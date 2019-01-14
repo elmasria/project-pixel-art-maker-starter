82fed6a6f8e2abd4e9f2b86f3d321a819d81b773
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2019-01-14 05:10:49 +0400

    feat: create grid

--- designs.js
+++ designs.js
@@ -1,8 +1,11 @@
+"use strict";
+
 // Select color input
 let inputColor = document.getElementById('colorPicker');
 // Select size input
 let inputSizeHeight = document.getElementById('inputHeight');
 let inputSizeWidth = document.getElementById('inputWidth');
+
 
 // When size is submitted by the user, call makeGrid()
 document.getElementById("sizePicker")
@@ -10,10 +13,42 @@ document.getElementById("sizePicker")
 
 function makeGrid(e) {
 
-  // Your code goes here!
-  let inputColorValue = inputColor.value;
-  let inputSizeHeightValue = inputSizeHeight.value;
-  let inputSizeWidthValue = inputSizeWidth.value;
   e.preventDefault();
 
+  // Select grid Canvas
+  let tblGrid = document.getElementById("pixelCanvas");
+
+  // Get input values
+  let inputSizeHeightValue = inputSizeHeight.value;
+  let inputSizeWidthValue = inputSizeWidth.value;
+
+  tblGrid.innerHTML = "";
+
+  const fragment = document.createDocumentFragment();
+  // loop over the height size to create the table row
+  for (var i = 0; i < inputSizeHeightValue; i++) {
+    let gridRow = document.createElement("tr");
+
+    // loop over the width size to create table cell
+    for (var z = 0; z < inputSizeWidthValue; z++) {
+      let gridCell = document.createElement("td");
+      Object.assign(gridCell, {
+        className: 'canvas-cell',
+      });
+      gridCell.addEventListener('click', (event) => {
+        colorCell(event, gridCell);
+      });
+      gridRow.appendChild(gridCell);
+    }
+
+    fragment.appendChild(gridRow);
+  }
+  tblGrid.appendChild(fragment);
+
+}
+
+// style the canvas grid cell
+function colorCell(e, el) {
+  let inputColorValue = inputColor.value;
+  el.style.backgroundColor = inputColorValue;
 }
