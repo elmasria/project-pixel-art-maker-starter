376fe7f4ebd13ea71091d8d4f61d17f6452b6b3b
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2019-01-14 04:48:17 +0400

    feat: select required inputs input

--- designs.js
+++ designs.js
@@ -1,10 +1,19 @@
 // Select color input
+let inputColor = document.getElementById('colorPicker');
 // Select size input
+let inputSizeHeight = document.getElementById('inputHeight');
+let inputSizeWidth = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
+document.getElementById("sizePicker")
+  .addEventListener("submit", makeGrid, false);
 
-function makeGrid() {
+function makeGrid(e) {
 
-// Your code goes here!
+  // Your code goes here!
+  let inputColorValue = inputColor.value;
+  let inputSizeHeightValue = inputSizeHeight.value;
+  let inputSizeWidthValue = inputSizeWidth.value;
+  e.preventDefault();
 
 }
